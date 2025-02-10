23224fb4551753089fbb0227455e03d1029f662f
test <test@Pipeline-1.example.com>
2025-02-10 09:37:38 +0000

    pushing

--- ui.py
+++ ui.py
@@ -5,7 +5,7 @@ from langchain_cohere import ChatCohere
 
 
 
-st.title("Cohere Chat Bot - v14")
+st.title("Cohere Chat Bot - v014")
 
 # Initialize chat history
 if "messages" not in st.session_state:
